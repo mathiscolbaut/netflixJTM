cc8e31ab88f9cf60da16b14ade89eafe8b66e737
mathiscolbaut <mathiscolbaut@gmail.com>
2022-12-08 15:24:56 +0100

    systeme de recherche
    systeme de recherche ok
    pour film 98%
    pour série 50%

--- netflixjtm/src/Controller/RechercheController.php
+++ netflixjtm/src/Controller/RechercheController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Controller;
+use Symfony\Component\HttpClient\CurlHttpClient;
+use Symfony\Component\HttpClient\NativeHttpClient;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,62 +11,105 @@ use Symfony\Component\Routing\Annotation\Route;
 class RechercheController extends AbstractController
 {
     /**
-     * @Route("/recherche", name="app_recherche")
+     * @Route("/recherchefilm", name="app_recherchefilm")
      */
     public function index(): Response
     {
         $titre = $_GET["titre"];
-
         $rechercheForApi = str_replace(' ', '$', $titre);
-        $urlImage= "https://image.tmdb.org/t/p/w500/";
-        $curl = curl_init('https://api.themoviedb.org/3/search/multi?api_key=1d3427f37b6eee91dcbf8fd38e04f6c9&language=fr&query='.$rechercheForApi.'&page=1&include_adult=true');
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        $data = curl_exec($curl);
-    
-        if($data == false)
-        {
-            var_dump(curl_error($curl));
-        }
-        else{
-            $data = json_decode($data,true);
-            $totalResults =$data['total_results'];
-            $compteur =0;
-            echo '<pre>';
-    
-    
-    
-                for ($i = -3; $i <= $totalResults; $i++) {
-    
-                        echo $data['results'][''.$compteur.'']['name'];
-                        echo $data['results'][''.$compteur.'']['title'];
-                        echo '<br>';
-    
-                        $url = $data['results'][''.$compteur.'']['poster_path'];
-                        $url = $urlImage . $url;
-                        echo '<img src="' . $url . '" alt="ScanLine" width="10%" height="30%" border="0" />';
-                        echo '<br>';
-                        echo '<br>';
-                        echo '<input  type="button" name="Telecharger" value="TELECHARGER" </input>';
-    
-    
-                        echo '<br>';
-                        echo '<br>';
-                        $totalResults = $totalResults-1;
-                        $compteur=$compteur+1;
-    
-                    }
-    
-            echo '</pre>';
-    
-        }
-       // curl_close($curl);
+        $client = new CurlHttpClient();
         
-        
+        $response = $client->request('GET', 'https://api.themoviedb.org/3/search/multi?api_key=1d3427f37b6eee91dcbf8fd38e04f6c9&language=fr&query='.$rechercheForApi.'&include_adult=true',[ 'verify_peer' => false,]);
+        $content = $response->toArray();
+
+        $result = $content['results'];
+       
+       for ($i=0; $i < max(array_keys($result)) ; $i++) { 
+            if(empty($result[$i]['title']))
+            {
+                $result[$i]['title'] = $result[$i]['title'] = "fefzjfkjzenjfezkjnfzeenknk";
+            }
+            if(empty($result[$i]['backdrop_path']))
+            {
+                $result[$i]['backdrop_path'] = $result[$i]['backdrop_path'] = "https://media.giphy.com/media/baPIkfAo0Iv5K/giphy.gif";
+            }
+            else{
+                $result[$i]['backdrop_path']= "https://image.tmdb.org/t/p/w500/".$result[$i]['backdrop_path'];
+            }
+            if(empty($result[$i]['overview']))
+            {
+                $result[$i]['overview'] = $result[$i]['overview'] = "Il n'y a pas de description";
+                
+            }
             
+
+
+       }        
         return $this->render('recherche/index.html.twig', [
             'controller_name' => 'RechercheController',
             'titre' => $rechercheForApi,
+            'data' => $result,
+        ]);
+    }
+
+
+ /**
+     * @Route("/rechercheserie", name="app_rechercheserie")
+     */
+    public function serie(): Response
+    {
+        $titre = $_GET["titre"];
+        $rechercheForApi = str_replace(' ', '$', $titre);
+        $client = new CurlHttpClient();
+        
+        $response = $client->request('GET', 'https://api.themoviedb.org/3/search/tv?api_key=1d3427f37b6eee91dcbf8fd38e04f6c9&language=fr-FR&query='.$rechercheForApi.'&include_adult=true',[ 'verify_peer' => false,]);
+        $content = $response->toArray();
+       
+        $result = $content['results'];
+       
+       for ($i=0; $i < max(array_keys($result)) ; $i++) { 
+            if(empty($result[$i]['name']))
+            {
+                $result[$i]['name'] = $result[$i]['name'] = "fefzjfkjzenjfezkjnfzeenknk";
+            }
+            if(empty($result[$i]['backdrop_path']))
+            {
+                $result[$i]['backdrop_path'] = $result[$i]['backdrop_path'] = "https://media.giphy.com/media/baPIkfAo0Iv5K/giphy.gif";
+            }
+            else{
+                $result[$i]['backdrop_path']= "https://image.tmdb.org/t/p/w500/".$result[$i]['backdrop_path'];
+            }
+            if(empty($result[$i]['overview']))
+            {
+                $result[$i]['overview'] = $result[$i]['overview'] = "Il n'y a pas de description";
+                
+            }
+            
+
+
+       }
+    
+
+           
+        
+            
+        return $this->render('recherche/rechercheserie.html.twig', [
+            'controller_name' => 'RechercheController',
+            'titre' => $rechercheForApi,
+            'data' => $result,
+        ]);
+    }
+    /**
+     * @Route("/voirplus/{idSerie}", name="app_voirplus")
+     */
+    public function voirplus(string $idSerie): Response
+    {
+       
+        
+
+        return $this->render('recherche/voirplus.html.twig', [
+            'controller_name' => 'RechercheController',
+            'idSerie'=>$idSerie,
             
         ]);
     }
