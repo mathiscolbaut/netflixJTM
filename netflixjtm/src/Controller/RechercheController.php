1b4d83b59e29ed47d18930d1b7d10c1bd90e800b
mathiscolbaut <mathiscolbaut@gmail.com>
2025-03-03 16:19:05 +0100

    Update
    Mise en place du téléchargement

--- netflixjtm/src/Controller/RechercheController.php
+++ netflixjtm/src/Controller/RechercheController.php
@@ -1,125 +1,108 @@
 <?php
-
 namespace App\Controller;
 use Symfony\Component\HttpClient\CurlHttpClient;
-use Symfony\Component\HttpClient\NativeHttpClient;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class RechercheController extends AbstractController
 {
-
-  
-    #[Route('/recherchefilm', name: 'app_recherchefilm')]
-    public function index(): Response
+    #[Route('/film', name: 'app_film')]
+    public function index(Request $request): Response
     {
-        $titre = $_GET["titre"];
-        $rechercheForApi = str_replace(' ', '$', $titre);
+        $titre = $request->query->get('titre', '');
+        $passkey = "146bd42efc53d4e6128d431139e04cd2";
         $client = new CurlHttpClient();
-        
-        $response = $client->request('GET', 'https://api.themoviedb.org/3/search/multi?api_key=1d3427f37b6eee91dcbf8fd38e04f6c9&language=fr&query='.$rechercheForApi.'&include_adult=true',[ 'verify_peer' => false,]);
-        $content = $response->toArray();
 
-        $result = $content['results'];
-       
-       for ($i=0; $i < max(array_keys($result)) ; $i++) { 
-            if(empty($result[$i]['title']))
-            {
-                $result[$i]['title'] = $result[$i]['title'] = "Aucun Titre (Ne télécharge pas si tu ne veux pas voir ton compte BAN)";
-            }
-            if(empty($result[$i]['backdrop_path']))
-            {
-                $result[$i]['backdrop_path'] = $result[$i]['backdrop_path'] = "https://media.giphy.com/media/baPIkfAo0Iv5K/giphy.gif";
-            }
-            else{
-                $result[$i]['backdrop_path']= "https://image.tmdb.org/t/p/w500/".$result[$i]['backdrop_path'];
-            }
-            if(empty($result[$i]['overview']))
-            {
-                $result[$i]['overview'] = $result[$i]['overview'] = "Il n'y a pas de description";
-                
-            }
-            
+        $results = [];
+        if (!empty($titre)) {
+            $rechercheForApi = urlencode($titre);
+            $apiUrl = "https://www.sharewood.tv/api/$passkey/search?name=$rechercheForApi";
 
+            $response = $client->request('GET', $apiUrl, [
+                'headers' => ['Accept' => 'application/json'],
+                'verify_peer' => false,
+            ]);
 
-       }        
-        return $this->render('recherche/index.html.twig', [
-            'controller_name' => 'RechercheController',
-            'titre' => $rechercheForApi,
-            'data' => $result,
+            $results = $response->toArray() ?? [];
+        }
+
+        return $this->render('film/index.html.twig', [
+            'titre' => $titre,
+            'data' => $results,
         ]);
     }
 
-
- 
-   
-   
-    #[Route('/rechercheserie', name: 'app_rechercheserie')]
-    public function serie(): Response
+    #[Route('/search', name: 'app_film_search', methods: ['GET'])]
+    public function search(Request $request): JsonResponse
     {
-        $titre = $_GET["titre"];
-        $rechercheForApi = str_replace(' ', '$', $titre);
+        $titre = $request->query->get('titre', '');
+        $passkey = "146bd42efc53d4e6128d431139e04cd2";
         $client = new CurlHttpClient();
-        
-        $response = $client->request('GET', 'https://api.themoviedb.org/3/search/tv?api_key=1d3427f37b6eee91dcbf8fd38e04f6c9&language=fr-FR&query='.$rechercheForApi.'&include_adult=true',[ 'verify_peer' => false,]);
-        $content = $response->toArray();
-       
-        $result = $content['results'];
-       
-       for ($i=0; $i < max(array_keys($result)) ; $i++) { 
-            if(empty($result[$i]['name']))
-            {
-                $result[$i]['name'] = $result[$i]['name'] = "Aucun Titre (Ne télécharge pas si tu ne veux pas voir ton compte BAN)";
-            }
-            if(empty($result[$i]['backdrop_path']))
-            {
-                $result[$i]['backdrop_path'] = $result[$i]['backdrop_path'] = "https://media.giphy.com/media/baPIkfAo0Iv5K/giphy.gif";
-            }
-            else{
-                $result[$i]['backdrop_path']= "https://image.tmdb.org/t/p/w500/".$result[$i]['backdrop_path'];
-            }
-            if(empty($result[$i]['overview']))
-            {
-                $result[$i]['overview'] = $result[$i]['overview'] = "Il n'y a pas de description";
-                
-            }
-            
 
+        $results = [];
+        if (!empty($titre)) {
+            $rechercheForApi = urlencode($titre);
+            $apiUrl = "https://www.sharewood.tv/api/$passkey/search?name=$rechercheForApi";
 
-       }
-    
+            $response = $client->request('GET', $apiUrl, [
+                'headers' => ['Accept' => 'application/json'],
+                'verify_peer' => false,
+            ]);
 
-           
-        
-            
-        return $this->render('recherche/rechercheserie.html.twig', [
-            'controller_name' => 'RechercheController',
-            'titre' => $rechercheForApi,
-            'data' => $result,
-        ]);
+            $results = $response->toArray() ?? [];
+        }
+
+        return new JsonResponse($results);
     }
-    /**
-     * @Route("/voirplus/{idSerie}", name="app_voirplus")
-     */
-    public function voirplus(string $idSerie): Response
+
+    #[Route('/download', name: 'app_film_download', methods: ['POST'])]
+    public function download(Request $request): JsonResponse
     {
-        
-        
 
-        $client = new CurlHttpClient();
+        $url = $request->request->get('url');
+        $filename = $request->request->get('filename');
+        if (empty($filename)) {
+            $filename = basename(parse_url($url, PHP_URL_PATH)); // Récupère le vrai nom du fichier depuis l'URL
+        }
         
-        $response = $client->request('GET', 'https://api.themoviedb.org/3/tv/'.$idSerie.'?api_key=1d3427f37b6eee91dcbf8fd38e04f6c9&language=fr_FR',[ 'verify_peer' => false,]);
-        $content = $response->toArray();
-       
-        $seasons = $content['seasons'];
+        if (empty($url)) {
+            return new JsonResponse(['error' => 'URL de téléchargement manquante'], Response::HTTP_BAD_REQUEST);
+        }
 
-        return $this->render('recherche/voirplus.html.twig', [
-            'controller_name' => 'RechercheController',
-            'idSerie'=>$idSerie,
-            'seasons' => $seasons,
-            
-        ]);
+        // Vérifie que l'URL est correcte
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            return new JsonResponse(['error' => 'URL invalide : ' . $url], Response::HTTP_BAD_REQUEST);
+        }
+
+        $savePath = $this->getParameter('kernel.project_dir') . '/public/torrents/' . basename($filename);
+
+        try {
+            $client = new CurlHttpClient();
+            $response = $client->request('GET', $url, [
+                'headers' => ['Accept' => 'application/x-bittorrent'],
+                'verify_peer' => false,
+            ]);
+
+            // Vérifie si la réponse est valide
+            if ($response->getStatusCode() !== 200) {
+                return new JsonResponse(['error' => 'Échec du téléchargement. Code HTTP: ' . $response->getStatusCode()], Response::HTTP_BAD_REQUEST);
+            }
+
+            file_put_contents($savePath, $response->getContent());
+
+            return new JsonResponse([
+                'success' => true,
+                'message' => 'Téléchargement terminé',
+                'path' => '/torrents/' . basename($filename)
+            ]);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Erreur lors du téléchargement : ' . $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
+
+
+
 }
